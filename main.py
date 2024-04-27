abc4979f70a13addf5f2eda43c8f54917038ba33
Abdulmalik Almasud <mlk1321.aa@gmail.com>
2024-04-27 10:17:34 +0300

    add comments

--- main.py
+++ main.py
@@ -1,46 +1,69 @@
-# from snapchat_ads.client import SnapchatAdsAPI
 import random
 import sqlite3
+from snapchat_ads.client import SnapchatAdsAPI
 
 class UserSegmentPusher:
-    
+    """
+    This class is responsible for pushing user information to custom segments in Snapchat's ad platform.
+    It also handles storing and retrieving user information from a local SQLite database.
+    """
+
     def __init__(self, db_path):
+        # Initialize the UserSegmentPusher with the path to the SQLite database and Snapchat Ads API credentials.
         self.db_path = db_path
-        # self.api = SnapchatAdsAPI(client_id='client_id', client_secret='client_secret', refresh_token='refresh_token')
-        
+        self.api = SnapchatAdsAPI(client_id='client_id', client_secret='client_secret', refresh_token='refresh_token')
+
     def generate_user_info(self):
-        id = random.randint(1, 1000)
+        # Generate random user info. This is a placeholder for the user data.
         user_info = {
-            'id': id,
-            'username': 'User' + str(id),
-            'email': 'User' + str(id) + '@example.com'
+            'id': random.randint(1, 1000),
+            'name': 'User' + str(random.randint(1, 1000)),
+            'email': 'user' + str(random.randint(1, 1000)) + '@example.com'
         }
         return user_info
-    
+
     def store_user_info(self, user_info):
+        # Store user info in SQLite database.
         conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
-        c.execute("INSERT INTO users VALUES (?, ?, ?)", (user_info['id'], user_info['username'], user_info['email']))
+        c.execute("INSERT INTO users VALUES (?, ?, ?)", (user_info['id'], user_info['name'], user_info['email']))
         conn.commit()
         conn.close()
-        
+
     def get_all_users(self):
+        # Retrieve all users from the SQLite database. This could be modified to retrieve only specific groups of users.
         conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
         c.execute("SELECT * FROM users")
         users = c.fetchall()
         conn.close()
         return users
-    
+
+    def push_to_snapchat(self, user_info):
+        # Push user info to Snapchat ad platform. This is where the magic happens!
+        self.api.AudienceSegment.create(
+            advertiser_id='your_advertiser_id',
+            name=user_info['name'],
+            users=[user_info],
+            description='Segment for ' + user_info['name']
+        )
+
+    def update_snapchat_segment(self, segment_id, user_info):
+        # Update existing segment. This could be used to add new users to an existing segment, for example.
+        self.api.AudienceSegment.update(
+            segment_id=segment_id,
+            users=[user_info]
+        )
 
 if __name__ == "__main__":
-    pusher = UserSegmentPusher('./Ninja-Software-Engineer-Assignment/userinfo.db')
+    # Instantiate the UserSegmentPusher
+    pusher = UserSegmentPusher('users.db')
     
+    # Generate and store user info
     user_info = pusher.generate_user_info()
-    
     pusher.store_user_info(user_info)
     
-    all_users = pusher.get_all_users()
-    
-    print(all_users)
-
+    # Get all users and push them to Snapchat
+    users = pusher.get_all_users()
+    for user in users:
+        pusher.push_to_snapchat(user)
