ab88233f3e083ef2ab39efd1f0196f710143ccc1
Abdulmalik Almasud <mlk1321.aa@gmail.com>
2024-04-27 10:00:41 +0300

    add store_user_info and sqlite db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from snapchat_ads.client import SnapchatAdsAPI
 import random
+import sqlite3
 
 class UserSegmentPusher:
     
@@ -15,12 +16,20 @@ class UserSegmentPusher:
             'email': 'User' + str(id) + '@example.com'
         }
         return user_info
+    def store_user_info(self, user_info):
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        c.execute("INSERT INTO users VALUES (?, ?, ?)", (user_info['id'], user_info['username'], user_info['email']))
+        conn.commit()
+        conn.close()
     
 
 if __name__ == "__main__":
-    pusher = UserSegmentPusher('users.db')
+    pusher = UserSegmentPusher('./Ninja-Software-Engineer-Assignment/userinfo.db')
     
     user_info = pusher.generate_user_info()
+    
+    pusher.store_user_info(user_info)
     
     print(user_info)
 
